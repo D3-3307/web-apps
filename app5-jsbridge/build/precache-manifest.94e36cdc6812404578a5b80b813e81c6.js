6f5bda3343782fb349514789d0c79f0bd7ed0178
LiuuY <mail4c59@gmail.com>
2020-08-17 16:35:31 +0800

    app5-jsbridge

---
+++ app5-jsbridge/build/precache-manifest.94e36cdc6812404578a5b80b813e81c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f259df05a8a1eca1d8046e8834569cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21bbd501a500cde9e27",
+    "url": "/static/css/main.81b8d7f6.chunk.css"
+  },
+  {
+    "revision": "b78f80de12e6ae06cd53",
+    "url": "/static/js/2.023fca99.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.023fca99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21bbd501a500cde9e27",
+    "url": "/static/js/main.ed58cbb2.chunk.js"
+  },
+  {
+    "revision": "7c8693538bb51a957e90",
+    "url": "/static/js/runtime-main.1022905a.js"
+  }
+]);
